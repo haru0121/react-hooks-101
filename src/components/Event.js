0b7fa2e11fd5a5f077f46ea38d51de4bf9e039f9
shindou haruna <haruna_shindou@respawn.jp>
2021-11-14 19:59:23 +0900

    create delete operationLogs

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,12 +1,20 @@
 import React,{useContext} from 'react'
-import {DELETE_EVENT} from '../actions/index.js';
+import {DELETE_EVENT,ADD_OPERATION_LOG} from '../actions/index.js';
 import AppContext from '../contexts/AppContext.js';
+import { timeCurrentIso8601 } from '../utils.js';
 const Event=({event})=> {
     const {dispatch} = useContext(AppContext);
       const id =event.id;
       const handleClickDeletBtn=()=>{
         const result=window.confirm(id+"を削除しても良いですか");
-        if(result) dispatch({type:DELETE_EVENT,id:id});
+        if(result){
+          dispatch({type:DELETE_EVENT,id:id});
+          dispatch({
+            type:ADD_OPERATION_LOG,
+            description:"イベント("+id+")を削除しました",
+            operationAt:timeCurrentIso8601()
+          }) 
+        }
       }
       return(<tr>
         <td>{id}</td>
