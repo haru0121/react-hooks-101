145b90def1398a9a458e2777c91955a2ff26aad7
shindou haruna <haruna_shindou@respawn.jp>
2021-11-14 15:36:13 +0900

    create event form & event table

---
+++ src/components/App.js
@@ -0,0 +1,46 @@
+import React,{useEffect,useState} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+const App=(props)=> {
+
+  return (
+  <>
+		<div className="container-fluid">
+			<h4>イベント作成</h4>
+
+			<form>
+				<div className="form-group">
+					<label htmlFor="formEventTitle">タイトル</label>
+					<input id="formEventTitle" className="form-control"/>
+				</div>
+
+				<div className="form-group">
+					<label htmlFor="formEventBody">ボディー</label>
+					<textarea id="formEventBody" className="form-control"/>
+				</div>
+
+				<button className="btn btn-primary">作成</button>
+				<button className="btn btn-danger">削除</button>
+			</form>
+
+			<h4>イベント一覧</h4>
+			<table className="table table-hover">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>タイトル</th>
+						<th>ボディー</th>
+						<th></th>
+					</tr>
+				</thead>
+			</table>
+		</div>
+
+	</>
+  )
+}
+
+App.defaultProps={
+	name:'',
+	price:1000
+}
+export default App
